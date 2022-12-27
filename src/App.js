adcde17c8c03ccccca512757bf686b98dea6d7c7
Onias da Rocha <onias.filho@gmail.com>
2022-12-26 21:31:17 -0400

    sets up initial layout for CV

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { experiences, certificates } from './util/data';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-wrapper">
+      <div className="left-section"></div>
+      <div className="right-section">
+        <div className="about-me">
+          <h2 className='section-title'>about me</h2>
+          <p className="section-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque est ante, egestas dapibus vulputate sit amet, venenatis at nulla. Aenean condimentum nunc metus, ac sagittis justo eleifend non. Nulla in ullamcorper ante. Nam consectetur metus at faucibus tincidunt. In in eleifend risus, nec suscipit lorem. 
+            Aenean varius aliquam nibh maximus convallis. In egestas arcu diam, at suscipit nibh pulvinar ac. Cras vel nisl eu leo vestibulum dignissim. Sed ut tempus nisi. Mauris libero ipsum, luctus sit amet elit vel, scelerisque euismod libero. Ut iaculis ligula eget pretium molestie. Aliquam rutrum ipsum in enim dignissim, ac luctus ex tempor.
+          </p>
+        </div>
+        <div className="education">
+        <h2 className='section-title'>education</h2>
+        <h4 className='subtitle'>Federal University of Mato Grosso (UFMT)</h4>
+          <p className="section-text">
+          2017 - Current | Computer Information Systems
+          </p>
+        </div>
+        <div className="experience">
+        <h2 className='section-title'>experience</h2>
+          {experiences.map(experiencia => (
+            <>
+              <div className="experience-wrapper">
+                <div className="year-wrapper">
+                  {experiencia.timeFrame}
+                </div>
+                <div className="exp-info-wrapper">
+                  <p className="company-name">
+                    {experiencia.companyName}
+                  </p>
+                  <p className="job-title">
+                    {experiencia.jobTitle}
+                  </p>
+                  <p className="job-description">
+                    {experiencia.jobDescription}
+                  </p>
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
+        <div className="certificates">
+          <h2 className='section-title'>certificates</h2>
+          {certificates.map(certificado => (
+            <div className="certificate-wrapper">
+            <p className="certificate-name">{certificado.name}</p>
+            <div className="certificate-info">
+              {certificado.timeFrame} - {certificado.info}
+            </div>
+          </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/util/data.js
@@ -0,0 +1,40 @@
+const experiences = [
+    {
+        "timeFrame": "2021",
+        "companyName": "Mercado Livre",
+        "jobTitle": "back-end engineer (junior)",
+        "jobDescription": "API development and maintenance using Spring Boot/ Spring Framework. Multi-source applications, working with Data Lake (Alation) and SQLServer. Assist with architectural design on event driven models",
+    },
+    {
+        "timeFrame": "2020",
+        "companyName": "Softplan",
+        "jobTitle": "Full Stack Developer",
+        "jobDescription": "API development and maintenance using Spring Boot/ Spring Framework. Front-end development with AngularJS, React and JSP. Development of applications with multi-database usage (SQL Server, Oracle, PostgreSQL).",
+    },
+    {
+        "timeFrame": "2020",
+        "companyName": "State Secretariat of Finance",
+        "jobTitle": "Front-end Developer (Internship)",
+        "jobDescription": "Front-end development with React. Back-end development with Spring Boot doing API design and database administration, using Oracle 11g",
+    },
+]
+
+const certificates = [
+    {
+        "timeFrame": "2019",
+        "name": "TOELF ITP",
+        "info": "Score 613/677"
+    },
+    {
+        "timeFrame": "2015",
+        "name": "Java Web Development with JavaServer Faces",
+        "info": "TDS Technologies"
+    },
+    {
+        "timeFrame": "2012",
+        "name": "Web Design",
+        "info": "Microlins"
+    },
+]
+
+export {experiences, certificates};
